20f6d1cd2ad0a28d3b1690e569e73237360cdc09
Kanai Lomayani <k.mahenge@gmail.com>
2022-11-09 08:52:27 +0300

    Revert: env loading

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,12 +51,5 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-switch(PHP_OS){
-    case 'Linux':
-        $app->loadEnvironmentFrom('.env.production');
-        break;
-    case 'WINNT':
-        $app->loadEnvironmentFrom('.env.local');
-        break;
-};
+
 return $app;
